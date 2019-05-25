6c3c41765e38bb46c7d4d1159e6d0bc5b64198ab
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-05-25 12:20:22 +0200

    Fixed problem with infinity loop some times happening

--- src/authentication/Login.js
+++ src/authentication/Login.js
@@ -4,53 +4,39 @@ import 'firebase/auth';
 
 const Login = () => {
 	let [user, setUser] = useState(null);
+	let [displayName, setDisplayName] = useState(null);
 
 	// This auto log you in if you been logged in before, so remove for production version
-	firebase.auth().onAuthStateChanged(user => user ? setUser(user) : setUser(null));
+	firebase.auth().onAuthStateChanged(user => {
+		if (user) {
+			setUser(user);
+			setDisplayName(user.displayName);
+		}
+	});
 
 	let loginWithGoogle = () => {
-		console.log('login me!');
-
 		// change firebase.auth.Auth.Persistence.LOCAL to NONE for production version
 		firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-		.then(function() {
-  			var provider = new firebase.auth.GoogleAuthProvider();
-			firebase.auth().signInWithPopup(provider).then(function(result) {
-				if (result.credential) {
-					setUser(firebase.auth().currentUser);
-				}
+			.then(function () {
+				var provider = new firebase.auth.GoogleAuthProvider();
+				firebase.auth().signInWithPopup(provider).then(function (result) {
+					if (result.credential) {
+						setUser(firebase.auth().currentUser);
+					}
+				});
+			})
+			.catch(function (error) {
+				console.log('Firebase auth error: ', error);
 			});
-		})
-		.catch(function(error) {
-		// Handle Errors here.
-			console.log('Firebase auth error: ', error);
-			
-		});
 	};
 
-	let logout = () => {
-		firebase.auth().signOut().then(function() {
-			console.log('Sign-out successful. ');
-			setUser(null);
-		}).catch(function(error) {
-			console.log('Sign-out - An error happened.', error);
-		  });
+	let loginWithEmail = () => {
+
 	};
+
+	let logout = () => firebase.auth().signOut().then(() => setUser(null)).catch((error) => console.log('Sign-out error: ', error));
 
 	if (user != null) {
-		let displayName;
-		let photoURL;
-		// User is signed in.
-		user.providerData.forEach(function (profile) {
-			// console.log("Sign-in provider: " + profile.providerId);
-			// console.log("  Provider-specific UID: " + profile.uid);
-			// console.log("  Name: " + profile.displayName);
-			// console.log("  Email: " + profile.email);
-			// console.log("  Photo URL: " + profile.photoURL);
-			displayName = profile.displayName;
-			photoURL = profile.photoURL
-		  });
-		
 		return (
 			<div className="Login">
 				Welcome back {displayName}.
@@ -63,12 +49,13 @@ const Login = () => {
 			<div className="Login">
 				<button onClick={loginWithGoogle}>Login with google.</button>
 				<br />
-				<button>Login with email</button>
+				<button onClick={loginWithEmail}>Login with email</button>
+				<br />
 				you need to login.
 				<br />
 			</div>
 		);
 	}
-	}
+}
 
 export default Login;
